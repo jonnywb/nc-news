91528717a969a74bc88790c2f901b67a11d292a8
Jonathan Brierley <jonnywb@users.noreply.github.com>
2023-11-21 16:26:20 +0000

    fix connection.js

--- listen.js
+++ listen.js
@@ -1,5 +1,7 @@
-const app = require("./app");
+const app = require("./app.js");
 
-app.listen(9090, () => {
-  console.log("This app is listening on port 9090");
+const { PORT = 9090 } = process.env;
+
+app.listen(PORT, () => {
+  console.log(`This app is listening on ${PORT}...`);
 });
