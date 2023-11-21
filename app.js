4152e17b3ccea0504b19fab09fa1d73590b538f4
Jonathan Brierley <jonnywb@users.noreply.github.com>
2023-11-21 11:15:44 +0000

    add PATCH /api/articles/id and testing

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require("express");
 const app = express();
 
 const { getTopics } = require("./controllers/topics.controllers");
-const { getArticles, getArticleById } = require("./controllers/articles.controllers");
+const { getArticles, getArticleById, patchArticleById } = require("./controllers/articles.controllers");
 const { handle404, handleCustomError, handlePsqlError, handleServerError } = require("./error");
 const { getApi } = require("./controllers/api.controllers");
+
+app.use(express.json());
 
 app.get("/api", getApi);
 
@@ -12,6 +14,8 @@ app.get("/api/topics", getTopics);
 
 app.get("/api/articles", getArticles);
 app.get("/api/articles/:article_id", getArticleById);
+
+app.patch("/api/articles/:article_id", patchArticleById);
 
 app.all("*", handle404);
 
