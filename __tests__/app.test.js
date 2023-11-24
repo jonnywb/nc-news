adbe75c74c0c6c086f0959938f8c016781d778d7
Jonathan Brierley <jonnywb@users.noreply.github.com>
2023-11-24 13:53:08 +0000

    changed tests to fix git workflow test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -118,7 +118,7 @@ describe("GET /api/articles/:article_id", () => {
           topic: "mitch",
           author: "butter_bridge",
           body: "I find this existence challenging",
-          created_at: "2020-07-09T20:11:00.000Z",
+          created_at: expect.any(String),
           votes: 100,
           article_img_url:
             "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
@@ -294,7 +294,7 @@ describe("PATCH /api/articles/:article_id", () => {
           topic: "mitch",
           author: "butter_bridge",
           body: "I find this existence challenging",
-          created_at: "2020-07-09T20:11:00.000Z",
+          created_at: expect.any(String),
           votes: 103,
           article_img_url:
             "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
@@ -571,7 +571,7 @@ describe("PATCH /api/comments/:comment_id", () => {
           votes: 19,
           author: "butter_bridge",
           article_id: 9,
-          created_at: "2020-04-06T12:17:00.000Z",
+          created_at: expect.any(String),
         });
       });
   });
