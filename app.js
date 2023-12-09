cd86ab4a3b7d964ff41cf44ca6ef9da57cb0e4cd
Jonathan Brierley <jonnywb@users.noreply.github.com>
2023-12-09 21:57:18 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 const apiRouter = require("./routes/api-router");
 
 const { handle404, handleCustomError, handlePsqlError, handleServerError } = require("./controllers/error");
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", apiRouter);
